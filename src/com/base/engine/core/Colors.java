45da72ba3edd0eab5d6fbeba50f157131ce2df18
MasterMatthew <Mastamats@yahoo.com>
2016-04-03 03:26:26 -0400

    Colors!!!

---
+++ src/com/base/engine/core/Colors.java
@@ -0,0 +1,15 @@
+package com.base.engine.core;
+
+import com.base.engine.rendering.Texture;
+
+public class Colors 
+{
+	public static final Texture black = new Texture("black.png");
+	public static final Texture blue = new Texture("blue.png");
+	public static final Texture brown = new Texture("brown.png");
+	public static final Texture cyan = new Texture("cyan.png");
+	public static final Texture green = new Texture("green.png");
+	public static final Texture orange = new Texture("orange.png");
+	public static final Texture red = new Texture("red.png");
+	public static final Texture white = new Texture("white.png");
+}
